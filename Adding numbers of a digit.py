4dce1bce2b01291c7162874bf9dfe4262a8588c7
Anshdv <107092883+Anshdv@users.noreply.github.com>
2024-07-02 11:47:28 -0600

    Update Adding numbers of a digit.py

--- Adding numbers of a digit.py
+++ Adding numbers of a digit.py
@@ -1,5 +1,5 @@
 
-input_number = int(input('\nEnter a number of which you want to add its digits. Do not enter to many digits or your answer will be wrong: '))
+input_number = int(input('\nEnter a number of which you want to add its digits: '))
 number = input_number
 ans = 0
 new_number = 1
@@ -8,3 +8,5 @@ while new_number != 0:
     new_number = int(number / 10)
     number = new_number
 print(f'\nAdding up the digits of {input_number} equals {ans}.')
+
+# testing git
